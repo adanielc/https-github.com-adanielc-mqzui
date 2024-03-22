e303f096cf7709e78e3cc27eb13a25e8e8ed533e
adanielc <andresdaniel@gmail.com>
2024-03-22 18:06:27 +0100

    Initial Commit

---
+++ mquiz/mquizApp.swift
@@ -0,0 +1,17 @@
+//
+//  mquizApp.swift
+//  mquiz
+//
+//  Created by Daniel Castillo Bermudez on 22/3/24.
+//
+
+import SwiftUI
+
+@main
+struct mquizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
