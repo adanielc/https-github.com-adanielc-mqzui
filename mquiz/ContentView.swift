4dea553bbcd318e7a95679417b299961c4b69fed
adanielc <andresdaniel@gmail.com>
2024-03-24 10:07:13 +0100

    First Commit

--- mquiz/ContentView.swift
+++ mquiz/ContentView.swift
@@ -7,15 +7,56 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+class QuizManager: ObservableObject {
+    @Published var mockQuiestions = [
+        Question(title: "First question", answer: "A", options: ["A","B","C","D"]),
+        Question(title: "Second question", answer: "A", options: ["A","B","C","D"]),
+        Question(title: "Thrid question", answer: "A", options: ["A","B","C","D"]),
+        Question(title: "Fourth question", answer: "A", options: ["A","B","C","D"])
+    ]
+    func canSubmitQuiz() -> Bool {
+            return mockQuiestions.filter({$0.selection == nil}).isEmpty
+    }
+    func gradeQuiz() -> String {
+        var correct: CGFloat = 0
+        for question in mockQuiestions {
+            if question.answer == question.selection {
+                correct += 1
+            }
         }
-        .padding()
+        return "\((correct/CGFloat(mockQuiestions.count)) * 100) %"
+    }
+}
+
+struct ContentView: View {
+    @StateObject var manager = QuizManager()
+    var body: some View {
+        TabView {
+            ForEach(manager.mockQuiestions.indices, id:\.self) {
+                index in
+                VStack {
+                    Spacer()
+                    QuestionView(question: $manager.mockQuiestions[index])
+                    Spacer()
+                    if let lastQuestion = manager.mockQuiestions.last, lastQuestion.id == manager.mockQuiestions[index].id {
+                        Button {
+                            print(manager.canSubmitQuiz())
+                        } label: {
+                            Text("submit")
+                                .padding()
+                                .foregroundColor(.white)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                                .fill(Color("AppColor"))
+                                .frame(width: 300))
+                        }
+                        .buttonStyle(.plain)
+                        .disabled(!manager.canSubmitQuiz())
+                    }
+                }
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
     }
 }
 
---
+++ mquiz/QuestionView.swift
@@ -0,0 +1,58 @@
+//
+//  QuestionView.swift
+//  mquiz
+//
+//  Created by Daniel Castillo Bermudez on 22/3/24.
+//
+
+import SwiftUI
+
+struct Question: Identifiable {
+    let id = UUID()
+    let title: String
+    let answer: String
+    let options: [String]
+    var selection: String?
+}
+
+struct QuestionView: View {
+    @Binding var question: Question
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text(question.title)
+            ForEach(question.options, id:\.self) {option in
+                HStack {
+                    Button {
+                        question.selection = option
+                        print(option)
+                    } label: {
+                        if question.selection == option {
+                            Circle()
+                                
+                                .shadow(radius: 3)
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(Color("AppColor"))
+                        } else {
+                            Circle()
+                                .stroke()
+                                .shadow(radius: 3)
+                                .frame(width: 24, height: 24)
+                        }
+                      
+                }
+                    Text(option)
+                }
+            }
+        }
+        .foregroundColor(Color(uiColor: .secondaryLabel))
+        .padding(.horizontal, 40)
+        .frame(width: 700, height: 1000, alignment: .leading)
+        .background(Color(uiColor: .systemGray6))
+        .cornerRadius(20)
+        .shadow(color: Color(uiColor: .label).opacity(0.2), radius: 10)
+    }
+}
+
+#Preview {
+    QuestionView(question:.constant(Question(title: "In which year was the iPhone introduced?", answer: "2007", options: ["2006", "2007", "2008", "2009", "2010"])) )
+}
