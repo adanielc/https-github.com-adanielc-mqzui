f0cfe47779de4e13264600d9f19623fe9b917c79
Scoresby <andresdaniel@gmail.com>
2024-03-29 12:54:01 +0100

    Add Supabase

---
+++ mquiz/QuizManager.swift
@@ -0,0 +1,8 @@
+//
+//  QuizManager.swift
+//  mquiz
+//
+//  Created by Daniel Castillo Bermudez on 28/3/24.
+//
+
+import Foundation
---
+++ mquiz/ResultsView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultsView.swift
+//  mquiz
+//
+//  Created by Daniel Castillo Bermudez on 29/3/24.
+//
+
+import SwiftUI
+
+struct ResultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultsView()
+}
